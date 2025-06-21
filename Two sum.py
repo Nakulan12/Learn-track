54373a7c6fddc523cd5a12591bf6992873115bd1
Nakulan R <nakulanr307@gmail.com>
2025-06-21 22:51:52 +0530

    Update Two sum.py

--- Two sum.py
+++ Two sum.py
@@ -3,6 +3,4 @@ class Solution:
         seen = {} 
         for i, num in enumerate(nums):
             diff = target - num
-            if diff in seen:
-                return [seen[diff], i]
-            seen[num] = i
+            
